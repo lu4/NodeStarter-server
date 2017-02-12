e6ff0673b2db93b7019a25099bf4b00490199ee2
Denis Yaremov <denis.yaremov@gmail.com>
2017-02-12 04:27:01 +0200

    Minor fixes

--- src/app/route.ts
+++ src/app/route.ts
@@ -9,22 +9,6 @@ export interface IRoute {
     patch: core.RequestHandler | null;
     options: core.RequestHandler | null;
     head: core.RequestHandler | null;
-    checkout: core.RequestHandler | null;
-    copy: core.RequestHandler | null;
-    lock: core.RequestHandler | null;
-    merge: core.RequestHandler | null;
-    mkactivity: core.RequestHandler | null;
-    mkcol: core.RequestHandler | null;
-    move: core.RequestHandler | null;
-    mSearch: core.RequestHandler | null;
-    notify: core.RequestHandler | null;
-    purge: core.RequestHandler | null;
-    report: core.RequestHandler | null;
-    search: core.RequestHandler | null;
-    subscribe: core.RequestHandler | null;
-    trace: core.RequestHandler | null;
-    unlock: core.RequestHandler | null;
-    unsubscribe: core.RequestHandler | null;
 }
 
 export class Route implements IRoute {
@@ -36,20 +20,4 @@ export class Route implements IRoute {
     patch: core.RequestHandler | null = null;
     options: core.RequestHandler | null = null;
     head: core.RequestHandler | null = null;
-    checkout: core.RequestHandler | null = null;
-    copy: core.RequestHandler | null = null;
-    lock: core.RequestHandler | null = null;
-    merge: core.RequestHandler | null = null;
-    mkactivity: core.RequestHandler | null = null;
-    mkcol: core.RequestHandler | null = null;
-    move: core.RequestHandler | null = null;
-    mSearch: core.RequestHandler | null = null;
-    notify: core.RequestHandler | null = null;
-    purge: core.RequestHandler | null = null;
-    report: core.RequestHandler | null = null;
-    search: core.RequestHandler | null = null;
-    subscribe: core.RequestHandler | null = null;
-    trace: core.RequestHandler | null = null;
-    unlock: core.RequestHandler | null = null;
-    unsubscribe: core.RequestHandler | null = null;
 }
--- src/app/routing.service.ts
+++ src/app/routing.service.ts
@@ -34,21 +34,5 @@ export class RoutingService extends Route {
         if (route.patch) { this.expressRouterService.instance.route(path).patch(route.patch); }
         if (route.options) { this.expressRouterService.instance.route(path).options(route.options); }
         if (route.head) { this.expressRouterService.instance.route(path).head(route.head); }
-        if (route.checkout) { this.expressRouterService.instance.route(path).checkout(route.checkout); }
-        if (route.copy) { this.expressRouterService.instance.route(path).copy(route.copy); }
-        if (route.lock) { this.expressRouterService.instance.route(path).lock(route.lock); }
-        if (route.merge) { this.expressRouterService.instance.route(path).merge(route.merge); }
-        if (route.mkactivity) { this.expressRouterService.instance.route(path).mkactivity(route.mkactivity); }
-        if (route.mkcol) { this.expressRouterService.instance.route(path).mkcol(route.mkcol); }
-        if (route.move) { this.expressRouterService.instance.route(path).move(route.move); }
-        if (route.mSearch) { this.expressRouterService.instance.route(path)['m-search'](route.mSearch); }
-        if (route.notify) { this.expressRouterService.instance.route(path).notify(route.notify); }
-        if (route.purge) { this.expressRouterService.instance.route(path).purge(route.purge); }
-        if (route.report) { this.expressRouterService.instance.route(path).report(route.report); }
-        if (route.search) { this.expressRouterService.instance.route(path).search(route.search); }
-        if (route.subscribe) { this.expressRouterService.instance.route(path).subscribe(route.subscribe); }
-        if (route.trace) { this.expressRouterService.instance.route(path).trace(route.trace); }
-        if (route.unlock) { this.expressRouterService.instance.route(path).unlock(route.unlock); }
-        if (route.unsubscribe) { this.expressRouterService.instance.route(path).unsubscribe(route.unsubscribe); }
     }
 }
